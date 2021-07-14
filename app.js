e8bf491cf4b3f26edfc23ab55dc5f8933705addc
Ugric <epicugric@gmail.com>
2021-07-14 12:12:10 +0100

    added prefix, help menu and save / load game

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const snooze = milliseconds => new Promise((resolve) => setTimeout(resolve, mill
 disbut(client);
 
 const gamedata = {}
+const gameintervals = {}
+
+const prefix = "!cc"
 
 const upgradesdata = { clickers: { ps: 1, price: 10 }, grandmas: { ps: 10, price: 100 }, factories: { ps: 100, price: 1000 } }
 
@@ -49,7 +52,15 @@ client.on('clickButton', async (button) => {
                 gamedata[button.message.id].upgrades[2][1]++
             }
         } else if (button.id == "save") {
-            button.clicker.user.send("game saved, when you want to load the game, click this", new MessageAttachment(Buffer.from('some text', 'utf-8'), 'myfile.txt'))
+            const data = gamedata[button.message.id]
+            delete gamedata[button.message.id]
+            delete data.user
+            clearInterval(gameintervals[button.message.id])
+            delete gameintervals[button.message.id]
+            button.message.delete()
+            const databuffer = Buffer.from(JSON.stringify(data)).toString('base64')
+            button.clicker.user.send(`game saved, when you want to load the game do:\`\`\`${prefix} start ${databuffer}\`\`\``)
+            return
         }
         if (olddata !== JSON.stringify(gamedata[button.message.id])) {
             button.message.edit(embedrender(gamedata[button.message.id]))
@@ -58,41 +69,59 @@ client.on('clickButton', async (button) => {
 })
 client.on('message', async message => {
     if (message.author.bot) return
-    if (message.content === '!cc start') {
-        const tempgamedata = {
-            user: {
-                tag: message.author.tag,
-                id: message.author.id,
-                avatarURL: message.author.avatarURL()
-            }, cookies: 0, upgrades: [["clickers", 0], ["grandmas", 0], ["factories", 0]], interval: setInterval(() => {
+    const command = message.content.split(" ")
+    if (command.length > 0 && command[0] == prefix) {
+        command.shift()
+        if (command.length > 0 && command[0] == "start") {
+            command.shift()
+            const tempgamedata = {
+                user: {
+                    tag: message.author.tag,
+                    id: message.author.id,
+                    avatarURL: message.author.avatarURL()
+                }, cookies: 0, upgrades: [["clickers", 0], ["grandmas", 0], ["factories", 0]]
+            }
+            if (command.length > 0) {
+                try {
+                    const sentdata = JSON.parse(Buffer.from(command[0], 'base64').toString('ascii'))
+                    tempgamedata.cookies = sentdata.cookies
+                    tempgamedata.upgrades = sentdata.upgrades
+                } catch {
+                    message.channel.send(`could not load game from that game data are you sure you pasted it properly?`)
+                    return
+                }
+            }
+            const sentmessage = await message.channel.send(embedrender(tempgamedata), new disbut.MessageActionRow()
+                .addComponent(new disbut.MessageButton()
+                    .setStyle("red").setLabel("🍪")
+                    .setID(`click`)
+                ).addComponent(new disbut.MessageButton()
+                    .setStyle("red").setLabel("upgrade clickers")
+                    .setID(`clicker`)
+                ).addComponent(new disbut.MessageButton()
+                    .setStyle("red").setLabel("upgrade grandmas")
+                    .setID(`grandma`)
+                ).addComponent(new disbut.MessageButton()
+                    .setStyle("red").setLabel("upgrade factories")
+                    .setID(`factory`)
+                ).addComponent(new disbut.MessageButton()
+                    .setStyle("red").setLabel("save and close")
+                    .setID(`save`)
+                ));
+
+            gamedata[sentmessage.id] = tempgamedata
+            gameintervals[sentmessage.id] = setInterval(() => {
                 const olddata = JSON.stringify(gamedata[sentmessage.id])
                 for (let i = 0; i < gamedata[sentmessage.id].upgrades.length; i++) {
-                    gamedata[sentmessage.id].cookies += upgradesdata[gamedata[sentmessage.id].upgrades[i][0]].ps * gamedata[sentmessage.id].upgrades[i][1] * 2
+                    gamedata[sentmessage.id].cookies += upgradesdata[gamedata[sentmessage.id].upgrades[i][0]].ps * gamedata[sentmessage.id].upgrades[i][1] * 5
                 }
                 if (olddata !== JSON.stringify(gamedata[sentmessage.id])) {
                     sentmessage.edit(embedrender(gamedata[sentmessage.id]))
                 }
-            }, 2000)
+            }, 5000)
+        } else {
+            message.channel.send(`do \`${prefix} start\``)
         }
-        const sentmessage = await message.channel.send(embedrender(tempgamedata), new disbut.MessageActionRow()
-            .addComponent(new disbut.MessageButton()
-                .setStyle("red").setLabel("🍪")
-                .setID(`click`)
-            ).addComponent(new disbut.MessageButton()
-                .setStyle("red").setLabel("upgrade clickers")
-                .setID(`clicker`)
-            ).addComponent(new disbut.MessageButton()
-                .setStyle("red").setLabel("upgrade grandmas")
-                .setID(`grandma`)
-            ).addComponent(new disbut.MessageButton()
-                .setStyle("red").setLabel("upgrade factories")
-                .setID(`factory`)
-            ).addComponent(new disbut.MessageButton()
-                .setStyle("red").setLabel("save and close")
-                .setID(`save`)
-            ));
-
-        gamedata[sentmessage.id] = tempgamedata
     }
 });
 
